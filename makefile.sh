f312908c70a6a5c28ada1061b2322eb3fc9d6890
AGCTanaka <bluejb.bluejay@iloud.com>
2019-07-29 14:51:37 +0900

    test

---
+++ makefile.sh
@@ -0,0 +1,12 @@
+./configure \
+  --enable-fail-if-missing \
+  --with-features=huge \
+  --disable-selinux \
+  --enable-luainterp \
+  --enable-pythoninterp=dynamic \
+  --enable-python3interp=dynamic \
+  --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
+  --enable-cscope \
+  --enable-fontset \
+  --enable-multibyte \
+  vi_cv_path_ptyhon3=/usr/lib/python3.5
