6a5542841dced2a90403323cc047e2c3f07d1a0a
AGCTanaka <bluejb.bluejay@iloud.com>
2019-07-29 15:23:08 +0900

    test

---
+++ mecab_install.sh
@@ -0,0 +1,3 @@
+sudo apt-get install mecab libmecab-dev mecab-ipadic
+sudo aptitude install mecab-ipadic-utf8
+sudo apt-get install mecab-python3
--- python_setup.sh
+++ python_setup.sh
@@ -1,3 +1,5 @@
 curl -kL https://bootstrap.pypa.io/get-pip.py |sudo python3
 sudo pip install mecab-python3
+sudo pyenv install 3.6.2
+sudo pyenv global system 3.6.2
 
